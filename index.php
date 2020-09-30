4eba36619c351c50e5b02032a62a36cb6d362597
Alex Xiao <alex.hongquan.xiao@gmail.com>
2020-09-30 16:08:34 -0700

    push

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
-// $request = substr($_SERVER['REQUEST_URI'], 4);
-$request = $_SERVER['REQUEST_URI'];
-echo $request;
-switch ($request) {
-    case '/' :
-        require __DIR__ . './views/home.php';
-        break;
-    case '' :
-        require __DIR__ . './views/home.php';
-        break;
-    case '/about' :
-        require __DIR__ . './views/about.php';
-        break;
-    case '/news' :
-        require __DIR__ . './views/news.php';
-        break;
-    case '/product' :
-        require __DIR__ . './views/product.php';
-        break;
-    case '/contact' :
-        require __DIR__ . './views/contact.php';
-        break;
-    default:
-        http_response_code(404);
-        echo 'Not Found';
-        break;
-}
 
+// Include the Router class
+require __DIR__ . '/vendor/autoload.php';
+
+// Create a Router
+$router = new \Bramus\Router\Router();
+
+// Custom 404 Handler
+$router->set404(function () {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+    echo '404, route not found!';
+});
+
+// Before Router Middleware
+$router->before('GET', '/.*', function () {
+    header('X-Powered-By: bramus/router');
+});
+
+// Static route: / (homepage)
+$router->get('/', function () {
+    echo '<h1>bramus/router</h1><p>Try these routes:<p><ul><li>/hello/<em>name</em></li><li>/blog</li><li>/blog/<em>year</em></li><li>/blog/<em>year</em>/<em>month</em></li><li>/blog/<em>year</em>/<em>month</em>/<em>day</em></li><li>/movies</li><li>/movies/<em>id</em></li></ul>';
+});
+
+// Static route: /hello
+$router->get('/hello', function () {
+    echo '<h1>bramus/router</h1><p>Visit <code>/hello/<em>name</em></code> to get your Hello World mojo on!</p>';
+});
+
+// Thunderbirds are go!
+$router->run();
