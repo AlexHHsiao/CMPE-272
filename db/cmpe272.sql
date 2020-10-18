73d56dd6d21c7330a579816f58622d894ad3e487
Alex Xiao <alex.hongquan.xiao@gmail.com>
2020-10-17 20:58:03 -0700

    add sql

---
+++ db/cmpe272.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 18, 2020 at 05:07 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cmpe272`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `firstName` char(255) NOT NULL,
+  `lastName` char(255) NOT NULL,
+  `email` char(255) NOT NULL,
+  `address` char(255) NOT NULL,
+  `homePhone` char(255) NOT NULL,
+  `cellPhone` char(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`firstName`, `lastName`, `email`, `address`, `homePhone`, `cellPhone`) VALUES
+('Alex', 'Xiao', 'ddd@gmailcom', '123456', '123456', '123456');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- db/db.php
+++ db/db.php
@@ -2,15 +2,25 @@
 $hn = "localhost";
 $un = "root";
 $pw = "";
-$db = "cs272";
+$db = "user";
 $connection = new mysqli($hn, $un, $pw);
 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 
-$connection->query("CREATE DATABASE $db");
+ $connection->query("CREATE DATABASE cmpe272");
 
+
+$sqlSource = file_get_contents("./db/cmpe272.sql");
+mysqli_multi_query($connection,$sqlSource);
+
+$sql = "SELECT * FROM $db";  //edit your table name here
+$res = $connection->query($sql) or die($connection->error);;
+echo $res;
+while ($row = $res->fetch_assoc()) {
+    // print_r($row);
+}
 
 
 //$exists = $connection->query("SELECT name FROM $db");
