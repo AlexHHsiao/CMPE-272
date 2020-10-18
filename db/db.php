2593884e7a69b44b7ef77f720dd62064d3eb8fc7
Alex Xiao <alex.hongquan.xiao@gmail.com>
2020-10-18 15:17:39 -0700

    finish lab

--- db/db.php
+++ db/db.php
@@ -1,37 +1,18 @@
 <?php
-$hn = "localhost";
-$un = "root";
-$pw = "";
-$db = "user";
-$connection = new mysqli($hn, $un, $pw);
+$server = 'us-cdbr-east-02.cleardb.com';
+$username = 'b29064eaa430d7';
+$password = '10702318';
+$db = 'heroku_b9123864f1692c6';
 
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+//$server = 'localhost';
+//$username = 'root';
+//$password = '';
+//$db = 'heroku_b9123864f1692c6';
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
- $connection->query("CREATE DATABASE cmpe272");
-
-
-$sqlSource = file_get_contents("./db/cmpe272.sql");
-mysqli_multi_query($connection,$sqlSource);
-
-$sql = "SELECT * FROM $db";  //edit your table name here
-$res = $connection->query($sql) or die($connection->error);;
-echo $res;
-while ($row = $res->fetch_assoc()) {
-    // print_r($row);
-}
-
-
-//$exists = $connection->query("SELECT name FROM $db");
-//if ($exists->num_rows === 0) {
-//    echo 'xxxx';
-////    $query = "INSERT INTO users VALUES('$fn', '$sn', '$un', '$pw')";
-////    $result = $connection->query($query);
-////    if (!$result) die($connection->error);
-//}
-
-
-
-
-
+$GLOBALS['conn'] = $conn;
